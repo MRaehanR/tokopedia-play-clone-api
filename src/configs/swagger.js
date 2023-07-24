6526a567c042a51ee70c84834ce1bf2680abf335
MRaehanR <raehanrobban2@gmail.com>
2023-07-24 13:17:43 +0700

    chore: change swagger.json to js

--- src/configs/swagger.js
+++ src/configs/swagger.js
@@ -1,9 +1,10 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
-import fs from "fs";
+import swaggerDoc from "../../docs/swagger.js";
+// import fs from "fs";
 
 const swaggerRoute = express.Router();
-const swaggerDoc = JSON.parse(fs.readFileSync("docs/swagger.json"));
+// const swaggerDoc = JSON.parse(fs.readFileSync("docs/swagger.json"));
 
 swaggerRoute.use("/api", swaggerUi.serve);
 swaggerRoute.get("/api", swaggerUi.setup(swaggerDoc));
